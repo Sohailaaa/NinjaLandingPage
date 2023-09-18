e542708cde8d92cbcbcfce9412e4e463b2f4d854
Sohailaaa <98600518+Sohailaaa@users.noreply.github.com>
2023-09-18 12:14:07 +0300

    added

---
+++ components/About/OurVision.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const OurVision = () => {
+    return (<>
+      <h3 className='span' style={{textAlign:'center',marginBottom:'90px',color:'#F0702F',backgroundColor:'transparent'}}> How works ?</h3>
+			<div>
+
+        <img src="/images/works1.png" alt="logo" style={{width:'50%'}}/>
+        <img src="/images/works2.png" alt="logo" style={{width:'50%'}}/>
+
+
+		</div></>
+    )
+}
+
+export default OurVision;
